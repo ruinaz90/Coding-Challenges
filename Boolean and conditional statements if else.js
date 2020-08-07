66f3063c61ffe5677fe0e547f848b145c23ad454
ruinaz90 <rzhao9002@gmail.com>
2020-08-06 20:00:00 -0600

    8kyu

---
+++ Boolean and conditional statements if else.js
@@ -0,0 +1,5 @@
+function trueOrFalse(val){
+    if (val)    return 'true';             
+    else     return 'false';
+  }
+  //https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript

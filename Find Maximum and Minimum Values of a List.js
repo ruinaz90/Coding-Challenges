f3469d4422ee2d6ac635fe89f7e9cc845092c110
ruinaz90 <52875611+ruinaz90@users.noreply.github.com>
2022-03-31 06:55:47 -0400

    Add files via upload

---
+++ Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,9 @@
+var min = function(list){
+    list = list.sort((a, b) => a - b)
+    return list[0];
+}
+
+var max = function(list){
+    list = list.sort((a, b) => a - b)
+    return list[list.length - 1];
+}
---
+++ count the monkeys.js
@@ -0,0 +1,8 @@
+function monkeyCount(n) {
+// your code here
+  let monkeyArr = []
+  for(let i = 1; i <= n; i++) {
+    monkeyArr.push(i)
+  }
+  return monkeyArr
+}

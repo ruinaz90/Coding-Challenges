0e56691effa12729abb80bcc23d3b1e0c897afb6
ruinaz90 <52875611+ruinaz90@users.noreply.github.com>
2022-04-08 06:53:30 -0400

    Create Traffic light.py
    Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to. For example, update_light('green') should return 'yellow'.

---
+++ Traffic light.py
@@ -0,0 +1,8 @@
+def update_light(current):
+    # Your code here.
+    if current == "red":
+        return "green"
+    elif current == "green":
+        return "yellow"
+    elif current == "yellow":
+        return "red"

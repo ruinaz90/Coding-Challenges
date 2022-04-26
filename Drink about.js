35b2dd972a2de720c6c5d4ecfc6334b4dd3336ff
ruinaz90 <52875611+ruinaz90@users.noreply.github.com>
2022-04-26 06:26:44 -0400

    Create Drink about.js
    https://www.codewars.com/kata/56170e844da7c6f647000063/solutions/javascript

---
+++ Drink about.js
@@ -0,0 +1,6 @@
+function peopleWithAgeDrink(old) {
+  if(old < 14) return "drink toddy"
+  else if(old < 18 && old > 13) return "drink coke"
+  else if(old < 21 && old > 17) return "drink beer"
+  else if(old > 20) return "drink whisky"
+};

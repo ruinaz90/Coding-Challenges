75f937560f0e79d3cda14615224767506b247f27
ruinaz90 <rzhao9002@gmail.com>
2022-05-26 10:15:02 +0000

    8kyu challenge

---
+++ Exclamation marks.js
@@ -0,0 +1,7 @@
+function remove (string) {
+    //coding and coding....
+    if(string[string.length - 1] === "!")
+      return string.slice(0, -1)
+    return string
+  }
+  //https://www.codewars.com/kata/57fae964d80daa229d000126/solutions/javascript

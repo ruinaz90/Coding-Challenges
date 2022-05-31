f94cdf3a18fe9d56ce66051b08c42e6edc5a02f2
ruinaz90 <rzhao9002@gmail.com>
2022-05-31 10:19:10 +0000

    7kyu

---
+++ 7kyu/JavaScript Array Filter.js
@@ -0,0 +1,5 @@
+function getEvenNumbers(numbersArray){
+    // filter out the odd numbers
+    return numbersArray.filter(number => number % 2 === 0)
+  }
+//https://www.codewars.com/kata/514a6336889283a3d2000001/train/javascript

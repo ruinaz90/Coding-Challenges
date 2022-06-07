76a41aaea97904c99074c606ce0046fb2dda4713
ruinaz90 <rzhao9002@gmail.com>
2022-06-07 10:22:52 +0000

    8kyu
    https://www.codewars.com/kata/56530b444e831334c0000020/train/javascript

---
+++ Determine offspring sex.js
@@ -0,0 +1,3 @@
+function chromosomeCheck(sperm) {
+    return sperm.toLowerCase() === "xx" ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son."
+  }

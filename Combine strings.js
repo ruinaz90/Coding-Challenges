961feb0624b47684a1851816f232d6e88a70551f
ruinaz90 <rzhao9002@gmail.com>
2022-07-07 10:16:48 +0000

    8kyu

---
+++ Combine strings.js
@@ -0,0 +1,5 @@
+// Create the combineNames function here
+function combineNames(first, last) {
+  return `${first} ${last}`
+}
+//https://www.codewars.com/kata/55f73f66d160f1f1db000059/train/javascript

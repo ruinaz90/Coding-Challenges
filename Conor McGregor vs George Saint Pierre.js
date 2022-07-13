4885a660b47a08aeac3dae56be89de8a4128c9b9
ruinaz90 <rzhao9002@gmail.com>
2022-07-13 05:13:37 +0000

    8kyu

---
+++ Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,8 @@
+var quote = function(fighter) {
+    // your code here
+    switch(fighter.toLowerCase()) {
+        case "conor mcgregor": return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+        case "george saint pierre": return "I am not impressed by your performance."
+    }
+  };
+  //https://www.codewars.com/kata/582dafb611d576b745000b74/train/javascript

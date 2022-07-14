48c693f194f75fb17923bd4916dfef20eddeabbb
ruinaz90 <rzhao9002@gmail.com>
2022-07-14 08:14:14 +0000

    8kyu

---
+++ Conditional statement--switch.js
@@ -0,0 +1,19 @@
+function howManydays(month){
+    var days;
+    switch (month){
+      case 1: return 31
+      case 2: return 28
+      case 3: return 31
+      case 4: return 30
+      case 5: return 31
+      case 6: return 30
+      case 7: return 31
+      case 8: return 31
+      case 9: return 30
+      case 10: return 31
+      case 11: return 30
+      case 12: return 31
+    }
+    return days;
+  }
+  //https://www.codewars.com/kata/572059afc2f4612825000d8a/train/javascript

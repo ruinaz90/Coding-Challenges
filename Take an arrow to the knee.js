896e5a531d1697ac3e791fbeb2eb7f068400e2cf
ruinaz90 <rzhao9002@gmail.com>
2022-07-31 11:02:04 +0000

    8kyu

---
+++ Take an arrow to the knee.js
@@ -0,0 +1,4 @@
+var ArrowFunc = function(arr) {
+    return arr.map(element => String.fromCharCode(element)).join(''); //Complete this function
+  }
+  //https://www.codewars.com/kata/559f3123e66a7204f000009f/train/javascript

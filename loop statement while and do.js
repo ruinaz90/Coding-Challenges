d3421beaf56d2bc68032826b9fe1497a5edb4ff4
ruinaz90 <rzhao9002@gmail.com>
2022-08-04 10:18:04 +0000

    8kyu

--- loop statement while and do.js
+++ loop statement while and do.js
@@ -1,12 +1,14 @@
 function padIt(str,n){
-    for(let i = 0; i < n; i++) {
-      if(i % 2 === 0) {
-        str = '*' + str
-      }
-      else {
-        str = str + '*'
-      }
+  let i = 0
+  while(i < n) {
+    if(i % 2 === 0) {
+      str = '*' + str
     }
-    return str
+    else {
+      str = str + '*'
+    }
+    i++
   }
+  return str
+}
   //https://www.codewars.com/kata/57216d4bcdd71175d6000560/train/javascript

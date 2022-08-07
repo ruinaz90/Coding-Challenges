99a132ca82266c84baaaa42366d56d08689e8b71
ruinaz90 <rzhao9002@gmail.com>
2022-08-07 05:59:48 -0400

    Revert "8kyu"
    This reverts commit 66f3063c61ffe5677fe0e547f848b145c23ad454.

--- Boolean and conditional statements if else.js
+++
@@ -1,5 +0,0 @@
-function trueOrFalse(val){
-    if (val)    return 'true';             
-    else     return 'false';
-  }
-  //https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript

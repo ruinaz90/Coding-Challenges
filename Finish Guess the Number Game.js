e90d63489251f71b135d82a36628b8ebc1a1c878
ruinaz90 <arenzi28@gmail.com>
2022-08-10 05:28:40 -0400

    8kyu

---
+++ Finish Guess the Number Game.js
@@ -0,0 +1,16 @@
+class Guesser {
+  constructor(number, lives) {
+    this.number = number;
+    this.lives = lives;
+  }
+  
+  guess(n) {
+    if (!this.lives) throw "Error"
+    else if (n !== this.number) {
+      this.lives--
+      return false
+    }
+    else if (n === this.number) return true
+  }
+}
+//https://www.codewars.com/kata/568018a64f35f0c613000054/train/javascript

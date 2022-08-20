b35badfffcc450a37ddd1f1569af453d6468642c
ruinaz90 <rzhao9002@gmail.com>
2022-08-20 10:37:52 +0000

    8kyu

---
+++ The Ternary Operator.js
@@ -0,0 +1,6 @@
+// TODO: Refactor and shorten the function
+
+function describeAge(age) {
+    return "You're a(n) " + (age > 64 ? "elderly" : age > 17 ? "adult" : age > 12 ? "teenager" : "kid")
+  }
+  //https://www.codewars.com/kata/56f3f6a82010832b02000f38/train/javascript

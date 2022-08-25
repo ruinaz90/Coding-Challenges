a63d0d6275a04b18ed4c9e4f782a35e52fb18fc5
ruinaz90 <rzhao9002@gmail.com>
2022-08-25 11:28:57 +0000

    8kyu string

---
+++ Is the string uppercase.js
@@ -0,0 +1,5 @@
+String.prototype.isUpperCase = function() {
+    // your code here
+    return this == this.toUpperCase()
+  }
+  //https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/solutions/javascript

f188a3325d7c5a165830c94ebfb4dc7f25003d7f
ruinaz90 <rzhao9002@gmail.com>
2022-08-29 10:01:35 +0000

    8kyu

---
+++ A bugs trilogy Episode 1.js
@@ -0,0 +1,12 @@
+function yourFutureCareer() {
+	const career = Math.random()
+		if (career <= 0.32)
+			return "FrontEnd Developer"
+		 else if (career <= 0.65)
+			return "BackEnd Developer"
+		else
+			return 'Full-Stack Developer'
+}
+
+yourFutureCareer();
+//https://www.codewars.com/kata/562e98755e9214cd2500003d/train/javascript

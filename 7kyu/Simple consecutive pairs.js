c9dd7121ab9dc56634abf8dd6cfa56cc736cac22
ruinaz90 <rzhao9002@gmail.com>
2022-09-08 10:15:03 +0000

    arrays 7kyu

---
+++ 7kyu/Simple consecutive pairs.js
@@ -0,0 +1,9 @@
+function pairs(ar){
+    let count = 0
+    for(let i = 0; i < ar.length; i+=2) {
+        if(ar[i] + 1 === ar[i + 1] || ar[i] - 1 === ar[i + 1])
+            count++
+    }
+    return count
+}
+//https://www.codewars.com/kata/5a3e1319b6486ac96f000049/solutions/javascript

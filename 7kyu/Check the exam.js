f30193d648a199326e1486eb40c54a84489c76a9
ruinaz90 <52875611+ruinaz90@users.noreply.github.com>
2022-09-25 05:03:13 -0400

    array 7 kyu
    https://www.codewars.com/kata/5a3dd29055519e23ec000074/solutions/javascript

---
+++ 7kyu/Check the exam.js
@@ -0,0 +1,12 @@
+function checkExam(array1, array2) {
+    let score = 0
+    for(let i = 0; i < array1.length; i++) {
+        if(array1[i] == array2[i])
+            score += 4
+        else if(array2[i] == "")
+            score += 0
+        else score--
+    }
+    if(score < 0) return 0
+    else return score
+}

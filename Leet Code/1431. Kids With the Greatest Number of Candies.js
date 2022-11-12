97c6f98267ad6b8720b72d8b8e3acec2d78bba66
ruinaz90 <52875611+ruinaz90@users.noreply.github.com>
2022-11-12 07:08:08 -0500

    Create 1431. Kids With the Greatest Number of Candies.js

---
+++ Leet Code/1431. Kids With the Greatest Number of Candies.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const max = Math.max(...candies)
+    return candies.map(item => item + extraCandies >= max)
+};

4069beec0603bd4a6464a7db12e2753bb692d366
ruinaz90 <52875611+ruinaz90@users.noreply.github.com>
2022-11-16 06:50:01 -0500

    Create 221115 sort odd.js

---
+++ huntober/Daily Coding Challenge/221115 sort odd.js
@@ -0,0 +1,7 @@
+function sortArray(arr){
+    const oddNums = arr.filter(element => element % 2 !== 0).sort((a, b) => a - b)
+    return arr.map(element => element % 2 === 0 ? element : oddNums.shift())
+}
+
+console.log(sortArray([7, 1]),'[ 1, 7]')
+console.log(sortArray([5, 8, 6, 3, 4]),'[ 3, 8, 6, 5, 4 ]')

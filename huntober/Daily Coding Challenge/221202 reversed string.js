0155322b5f9d8545eac54e5159830ec873aa365d
ruinaz90 <52875611+ruinaz90@users.noreply.github.com>
2022-12-03 07:34:46 -0500

    Create 221202 reversed string.js

---
+++ huntober/Daily Coding Challenge/221202 reversed string.js
@@ -0,0 +1,9 @@
+function reverse(string) {
+    let reversed = ""
+    for(let letter of string) {
+        reversed = letter + reversed
+    }
+    return reversed
+}
+
+//https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038

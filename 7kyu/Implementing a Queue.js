9740ae7cd88a96b150b324228acb3747e57f73b3
ruinaz90 <rzhao9002@gmail.com>
2023-02-08 06:33:44 -0500

    coding challenge

---
+++ 7kyu/Implementing a Queue.js
@@ -0,0 +1,19 @@
+var Queue = function() {
+    // implement your Queue constructor here
+    this.queue = []
+  };
+  
+  Queue.prototype.enqueue = function(item) {
+    // add item to the queue
+    this.queue.push(item)
+  };
+  
+  Queue.prototype.dequeue = function() {
+    // remove item from the front of the queue and return its value
+    this.queue.pop()
+  };
+  
+  Queue.prototype.size = function() {
+    // return number of items in queue so far
+    return this.queue.length
+  };

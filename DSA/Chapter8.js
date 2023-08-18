1d57f4035f17f39c653eb9e71a5ed200a198aba2
ruinaz90 <rzhao9002@gmail.com>
2023-08-18 07:07:27 -0400

    chapter 8

--- DSA/Chapter8.js
+++ DSA/Chapter8.js
@@ -15,3 +15,36 @@ function arrayIntersect(array1, array2) {
 
 const array1 = [1, 2, 3, 4, 5], array2 = [0, 2, 4, 6, 8]
 console.log(arrayIntersect(array1, array2))
+
+/* 2. Write a function that accepts an array of strings and returns the first duplicate value it finds. For example, if the array is ["a", "b", "c", "d", "c", "e", "f"], the function should return "c", since it’s duplicated within the array. */
+function firstDupe(strings) {
+    let hash = {}
+
+    for(let letter of strings) {
+        if(hash[letter])
+            return letter
+        else hash[letter] = true
+    }
+}
+
+const strings = ["a", "b", "c", "d", "c", "e", "f"]
+console.log(firstDupe(strings))
+
+/* 3. Write a function that accepts a string that contains all the letters of the alphabet except one and returns the missing letter. For example, the string, "the quick brown box jumps over a lazy dog" contains all the letters of the alphabet except the letter, "f". */
+function missingLetter(string) {
+    let hash = {}
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    for(let letter of string) {
+        hash[letter] = true
+    }
+
+    for(let letter of alphabet) {
+        if(!hash[letter]) return letter
+    }
+}
+
+const string = "the quick brown box jumps over a lazy dog"
+console.log(missingLetter(string))
+
+/* 4. Write a function that returns the first non-duplicated character in a string. For example, the string, "minimum" has two characters that only exist once—the "n" and the "u", so your function should return the "n", since it occurs first. */

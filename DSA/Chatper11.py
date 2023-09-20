6a8737b8710b9340af260a8146b6e0949dd57d6c
ruinaz90 <rzhao9002@gmail.com>
2023-09-20 06:36:26 -0400

    chapter 11 question 2

--- DSA/Chatper11.py
+++ DSA/Chatper11.py
@@ -4,5 +4,20 @@ def count_chars(array):
     if len(array) == 1:
         return len(array[0])
     return len(array[0]) + count_chars(array[1:])
+
 array_strings = ["ab", "c", "def", "ghij"]
 print(count_chars(array_strings))
+
+''' 11-2
+Use recursion to write a function that accepts an array of numbers and
+returns a new array containing just the even numbers. '''
+def even_nums(array):
+    if len(array) == 0:
+        return []
+    if array[0] % 2 == 0:
+        return [array[0]] + even_nums(array[1:])
+    else:
+        return even_nums(array[1:])
+
+array_nums = [1, 2, 3, 4, 5, 6]
+print(even_nums(array_nums))

ceb8d90067455743953d7493bd8fb28051504851
ruinaz90 <rzhao9002@gmail.com>
2023-09-21 06:51:10 -0400

    chapter 11 question 3

--- DSA/Chatper11.py
+++ DSA/Chatper11.py
@@ -21,3 +21,13 @@ def even_nums(array):
 
 array_nums = [1, 2, 3, 4, 5, 6]
 print(even_nums(array_nums))
+
+''' 11-3
+There is a numerical sequence known as "Triangular Numbers." The pattern begins as 1, 3, 6, 10, 15, 21, and continues onward with the Nth number in the pattern, which is N plus the previous number. For example, the 7th number in the sequence is 28, since it's 7 (which is N) plus 21 (the previous number in the sequence). Write a function that accepts anumber for N and returns the correct number from the series. That is, if the function was passed the number 7, the function would return 28. '''
+
+def triangular_nums(number):
+    if number == 1:
+        return 1
+    return number + triangular_nums(number - 1)
+
+print(triangular_nums(7))

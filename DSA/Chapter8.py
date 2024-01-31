9fc43739bfa5fab2da051f3fb2d02181368c8830
ruinaz90 <rzhao9002@gmail.com>
2024-01-31 06:24:27 -0500

    chapter 8

---
+++ DSA/Chapter8.py
@@ -0,0 +1,18 @@
+# 1.  Write a function that returns the intersection of two arrays. The intersection is a third array that contains all values contained within the first two arrays. For example, the intersection of [1, 2, 3, 4, 5] and [0, 2, 4, 6, 8] is [2, 4].
+
+def intersect(array1, array2):
+    array3 = []
+    hash_table = {}
+    
+    for i in array1:
+        hash_table[i] = True
+        
+    for i in array2:
+        if i in hash_table:
+            array3.append(i)
+    
+    return array3
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [0, 2, 4, 6, 8]
+print(intersect(arr1, arr2))
